401720309cdbc9d6152afb9ce59c1638864acfb9
a-fas <atsybulsky@gmail.com>
2016-06-22 15:02:04 +0000

    axios beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+import axios from 'axios';
 
 import MDPane from './mdpane.jsx';
 import {CostCenters, CostElements} from './mdobj.js';
@@ -32,7 +33,18 @@ class App extends Component {
   
   onClick = () => {
     
-    this.cCE.add({id: this.cCE.mdlist.length + 1, name: 'holy fuck it works !'});
+    axios.get('https://api.github.com/users/a-fas')
+      .then((response) => console.log(response))
+      .catch((error)   => console.log(error));
+    
+    // try {
+    //   const response = await axios.get('https://api.github.com/users/a-fas');
+    //   console.log(response.data);
+    // } catch (err) {
+    //   console.error(err);
+    // }    
+    
+    // this.cCE.add({id: this.cCE.mdlist.length + 1, name: 'holy fuck it works !'});
   }
 };
 

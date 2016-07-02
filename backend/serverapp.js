7dab21f4a6397322491849af8cc3f8c86e902eb5
a-fas <atsybulsky@gmail.com>
2016-07-02 21:22:27 +0300

    fix mongo

--- backend/serverapp.js
+++ backend/serverapp.js
@@ -1,18 +1,31 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
-// var routes     = require('./routes');
-// var mongoose   = require('mongoose');
+var routes     = require('./routes');
+var mongoose   = require('mongoose');
 
 function runApp() {
+  var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/mydb';
+
   // mongoose.connect('mongodb://localhost/mydb');
+
+  mongoose.connect(uristring, function (err, res) {
+    if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+      console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
 
   // express app will use body-parser to get data from POST
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
   // Main API route
-  // app.use('/api/v1', routes);
+  app.use('/api/v1', routes);
 
   app.get('/', function(request, response) {
     response.send('Hello wrold from tbt');
